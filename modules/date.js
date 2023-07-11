817e5526cdda51eb03e74e96bd8edf4a284cd2b1
2004-okari <116469172+2004-okari@users.noreply.github.com>
2023-07-11 08:01:08 +0000

    Fix clock

---
+++ modules/date.js
@@ -0,0 +1,13 @@
+import { DateTime } from './luxon.js';
+
+export const displayTime = () => {
+  const date = document.querySelector('.date-info');
+
+  const updateTime = () => {
+    const now = DateTime.now();
+    const formattedTime = now.toLocaleString(DateTime.DATETIME_MED_WITH_SECONDS);
+    date.innerHTML = formattedTime;
+  };
+  updateTime();
+  setInterval(updateTime, 1000);
+};
