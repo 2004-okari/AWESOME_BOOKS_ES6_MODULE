e17151342d38e96dda2fa6c93eefb675913c8baa
2004-okari <116469172+2004-okari@users.noreply.github.com>
2023-07-11 14:04:07 +0300

    Update bookCollection.js

--- modules/bookCollection.js
+++ modules/bookCollection.js
@@ -1,7 +1,7 @@
 // bookCollection.js
-import { Book } from './book.js';
+import Book from './book.js';
 
-export class BookCollection {
+export default class BookCollection {
   constructor() {
     this.collection = JSON.parse(localStorage.getItem('books')) || [];
     this.buttonAdd = document.querySelector('.add-button');
@@ -10,12 +10,21 @@ export class BookCollection {
     this.bookShelf = document.querySelector('.books-section');
 
     this.showBooks();
+
     this.buttonAdd.addEventListener('click', (e) => {
       e.preventDefault();
       this.addBook();
       this.saveBooks();
       this.showBooks();
       this.clearInput();
+    });
+
+    this.bookShelf.addEventListener('click', (e) => {
+      if (e.target.classList.contains('remove-button')) {
+        const bookElement = e.target.closest('.book-store');
+        const index = Array.from(this.bookShelf.children).indexOf(bookElement);
+        this.removeBook(index);
+      }
     });
   }
 
@@ -42,16 +51,16 @@ export class BookCollection {
   }
 
   showBooks() {
-    const displayBooks = this.collection.map((book, index) => `
+    const displayBooks = this.collection.map((book) => `
       <div class="book-store">
-        <div class="store-text">
-          <p class="book-title">"${book.title}"</p>
-          <p class="by-text">by</p>
-          <p class="book-author">${book.author}</p>
-        </div>
-        <button class="remove-button" onclick="bookCollection.removeBook(${index})">Remove</button> 
+      <div class="store-text">
+      <p class="book-title">"${book.title}"</p>
+      <p class="by-text">by</p>
+      <p class="book-author">${book.author}</p>
       </div>
-    `);
+      <button class="remove-button">Remove</button> 
+      </div>
+      `);
     this.bookShelf.innerHTML = displayBooks.join('');
   }
 }
